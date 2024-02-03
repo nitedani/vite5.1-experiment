6f4ac15c76674cebe928e858181978f66f8cd2bb
Dániel Horváth <horvath.daniel@lexunit.hu>
2024-02-03 02:17:32 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
 
 export default function App() {
-    const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-    return <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
+  return (
+    <button onClick={() => setCount((count) => count + 1)}>
+      count is {count}
     </button>
+  );
+}
+if (import.meta.hot) {
+  import.meta.hot.accept(() => {
+    import.meta.hot.invalidate();
+  });
 }
